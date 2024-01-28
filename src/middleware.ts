400dfdce5f7c11290a4e51aab56ff1416d9c0ffe
itsbluejelly <ronniedenzel0@gmail.com>
2024-01-28 22:46:38 +0300

    debugging session

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,6 +7,7 @@ export async function middleware(req: NextRequest): Promise<NextResponse<unknown
 
     const supabase = createMiddlewareClient({ req, res });
     const {data: {session}} = await supabase.auth.getSession();
+    console.log(session)
 
     if(whiteList.includes(req.nextUrl.pathname) && !session){
         return res
