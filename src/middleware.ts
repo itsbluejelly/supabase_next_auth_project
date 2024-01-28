c2af7a3b3618158ea90184826634885efe79933e
itsbluejelly <ronniedenzel0@gmail.com>
2024-01-28 22:34:19 +0300

    attempt 2 at restricting

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,19 +5,19 @@ export async function middleware(req: NextRequest): Promise<NextResponse<unknown
     const res: NextResponse<unknown> = NextResponse.next();
     const whiteList: string[] = ['/login', '/signup']
 
-    if(whiteList.includes(req.nextUrl.pathname)){
-        return res
-    }
-
     const supabase = createMiddlewareClient({ req, res });
     const {data: {session}} = await supabase.auth.getSession();
+
+    if(whiteList.includes(req.nextUrl.pathname) && !session){
+        return res
+    }
 
     if (!session) {
       return NextResponse.redirect(new URL('/login', req.url))
     }
 
-    if(whiteList.includes(req.nextUrl.pathname)){
-      return NextResponse.redirect('/')    
+    if(whiteList.includes(req.nextUrl.pathname) && session){
+      return NextResponse.rewrite(new URL('/', req.url))    
     }else{
       return res
     }
